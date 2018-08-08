71de9a1055e16e5874cd50de6f3823b305afc384
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-08 17:34:51 +0530

    v1

---
+++ m9/Functions and Objects Exercise-2/functions_and_objects_2.py
@@ -0,0 +1,27 @@
+"""
+#Exercise : Function and Objects Exercise-2
+#Implement a function that converts the given testList = [1, -4, 8, -9] into
+[2, -3, 9, -8]
+"""
+def apply_to_each(list_to_be_modified, function_to_be_modified):
+    """enumaration"""
+    for i in enumerate(list_to_be_modified):
+        list_to_be_modified[i] = function_to_be_modified(list_to_be_modified[i])
+    return list_to_be_modified
+
+def inc_inp(num):
+    """increment function"""
+    num = num + 1
+    return num
+
+def main():
+    """function calling"""
+    data = input()
+    data = data.split()
+    list1 = []
+    for j in data:
+        list1.append(int(j))
+    print(apply_to_each(list1, inc_inp))
+
+if __name__ == "__main__":
+    main()
