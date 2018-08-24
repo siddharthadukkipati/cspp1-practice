91bdb5e390e02361ad0adbd3555b79e78a5c4c1f
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-24 23:44:36 +0530

    gen_primes.py

---
+++ M15/practice/gen_primes.py
@@ -0,0 +1,22 @@
+#define the gen_primes function here
+def genPrimes():
+    num = 2
+    prime = True
+    while True:
+        if prime == True:
+            yield num
+        num = num + 1
+        for i in range(2,num):
+            if num % i == 0:
+               prime = False
+               break
+        else:
+            prime = True
+def main():
+	n = int(input())
+primenum = genPrimes()
+for i in range(n):
+   print(primenum.__next__())
+	
+if __name__== "__main__":
+	main()
