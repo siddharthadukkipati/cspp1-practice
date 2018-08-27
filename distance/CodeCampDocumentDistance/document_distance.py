5446f50dc7923d7e7b383c499950abec2b9eab26
Siddhartha Dukkipati <siddharthadukipati@msitprogram.net>
2018-08-27 09:26:02 +0530

    " -> 7 of 8 passed. pylint: 9.82/10 "

--- distance/CodeCampDocumentDistance/document_distance.py
+++ distance/CodeCampDocumentDistance/document_distance.py
@@ -25,15 +25,19 @@ def similarity(dict_1, dict_2):
     # print(inp_2)
     list_3 = inp_1 + inp_2
     # print(list_3)
-    """combining the 2 list into one and then using
-    the count key word to check for the count of that word"""
+    """
+    combining the 2 list into one and then using
+    the count key word to check for the count of that word
+    """
     a_dict = {}
     for word in list_3:
         if word not in load_stopwords(FILE_NAME).keys():
             a_dict[word] = (inp_1.count(word), inp_2.count(word))
     # print(a_dict)
     numerator, add_1, add_2 = 0, 0, 0
-    """for checking the values and calucating the value3"""
+    """
+    for checking the values and calucating the value3
+    """
     for key_check in a_dict:
         numerator += a_dict[key_check][0]*a_dict[key_check][1]
         add_1 += a_dict[key_check][0] ** 2
